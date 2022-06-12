af2c2677ae888fd21a2f0d0f0cd8939397c7e99b
Umut Toker <utoker@gmail.com>
2022-06-12 17:36:38 -0400

    typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { useState } from "react";
-import NewTodo from "../components/NewTodo";
-import Todos from "../components/Todos";
-import Todo from "../models/todo";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useState } from 'react';
+import NewTodo from '../components/NewTodo';
+import Todos from '../components/Todos';
+import Todo from '../models/todo';
 
 const Home: NextPage = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Todo App</title>
-        <meta name="Todo App" content="Created with Next.js & typescript" />
+        <meta name="Todo App" content="Created with Next.js & TypeScript" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NewTodo onAddTodo={addTodoHandler} />
